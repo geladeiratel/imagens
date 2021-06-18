14ec0c2a9b0fcda5306d35a53f1e53230dfa5793
simplecloudservlet <outrosdiasvirao@gmail.com>
2021-06-18 15:38:14 -0300

    Inicial commit

---
+++ aula3prog5/src/aula3prog5/Aula3Prog5.java
@@ -0,0 +1,17 @@
+package aula3prog5;
+
+public class Aula3Prog5 {			
+	
+	public static void main(String[] args) { 
+
+		int a[] = new int [10];
+		int b[] = {1,2,3,4,5}; 
+		
+		String c[] = new String[10];
+		
+		int d[][] = { {1,2}, {3,4} };
+		
+		
+	}
+
+}
