b15e20641c9606ba93f1585b0579c33046fc5ede
simplecloudservlet <outrosdiasvirao@gmail.com>
2021-06-24 10:05:40 -0300

    Untrack para iniciar os exercicios da aula5 sem os projetos da aula6.

--- aula6prog1/src/aula6prog1/A.java
+++
@@ -1,9 +0,0 @@
-package aula6prog1; 
-
-public class A {
-
-	public A() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- aula6prog1/src/aula6prog1/B.java
+++
@@ -1,9 +0,0 @@
-package aula6prog1;
-
-public class B extends A{
-
-	public B() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
