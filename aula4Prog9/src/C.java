9e2685053b54aed5d68919fcab4397b3e1b88032
simplecloudservlet <outrosdiasvirao@gmail.com>
2021-06-24 09:16:35 -0300

    Minor changes.

---
+++ aula4Prog9/src/C.java
@@ -0,0 +1,14 @@
+
+public class C {
+	
+	public C() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static String imprimir(){
+		return "Voce estah em C"; 
+	}
+	
+	
+	
+}
---
+++ aula4Prog9/src/Principal.java
@@ -0,0 +1,16 @@
+
+public class Principal {
+
+	public Principal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println(A.b.c.imprimir());
+		
+		
+
+	}
+
+}
