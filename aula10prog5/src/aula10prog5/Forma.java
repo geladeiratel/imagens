ad3a93f62a81c85678b5b980475574fe1df94320
simplecloudservlet <outrosdiasvirao@gmail.com>
2021-06-24 10:39:02 -0300

    aula10prog5

---
+++ aula10prog5/src/aula10prog5/Forma.java
@@ -0,0 +1,13 @@
+package aula10prog5;
+
+public interface Forma {
+
+	public final String teste="Valor Fixo"; 
+			
+	public abstract String toString();
+	
+	public abstract String getTipo();
+	
+	public abstract String getTeste();
+
+}
---
+++ aula10prog5/src/aula10prog5/Triangulo.java
@@ -0,0 +1,16 @@
+package aula10prog5;
+
+public class Triangulo implements Forma {
+	
+	public String getTipo(){
+		
+		return "Triangulo";
+	}
+	public String toString(){
+		return "Triangulo";
+	}
+	public String getTeste(){
+		return null;
+	}
+	
+}
