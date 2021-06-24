25c45ccb7a1ad230436743efac65aabce0f38015
simplecloudservlet <outrosdiasvirao@gmail.com>
2021-06-24 09:12:06 -0300

    aula4prog9

---
+++ aula4Prog9/src/A.java
@@ -0,0 +1,10 @@
+
+public class A {
+
+	public static B b;
+	public A() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
---
+++ aula4Prog9/src/B.java
@@ -0,0 +1,12 @@
+
+public class B {
+
+	public static C c;
+	public B() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static String imprimir(){
+		return "Voce estah em C"; 
+	}	
+}
