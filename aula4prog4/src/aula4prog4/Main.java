537f65dca97c1e0820d9608f7e9b431d40662694
simplecloudservlet <outrosdiasvirao@gmail.com>
2021-06-24 09:03:49 -0300

    aula4prog4

---
+++ aula4prog4/src/aula4prog4/Main.java
@@ -0,0 +1,12 @@
+package aula4prog4;
+
+public class Main {
+
+	public static void main(String [] args){
+		
+		Flor e = new Flor("rosa", 1, "vermelha", 29, 5, 2021);
+		
+		System.out.println(e);
+		
+	}
+}
