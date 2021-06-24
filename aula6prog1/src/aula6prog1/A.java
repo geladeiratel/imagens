930da1461144752e13f7d428a0e041da45c74890
simplecloudservlet <outrosdiasvirao@gmail.com>
2021-06-24 09:48:17 -0300

    aula6prog1

---
+++ aula6prog1/src/aula6prog1/A.java
@@ -0,0 +1,9 @@
+package aula6prog1;
+
+public class A {
+
+	public A() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ aula6prog1/src/aula6prog1/B.java
@@ -0,0 +1,9 @@
+package aula6prog1;
+
+public class B extends A{
+
+	public B() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
