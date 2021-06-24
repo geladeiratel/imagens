1f269b13a9b610ea2634e0d992a02936df6a4d06
simplecloudservlet <outrosdiasvirao@gmail.com>
2021-06-24 09:01:18 -0300

    aula4prog3

---
+++ aula4prog3/src/aula4prog3/Main.java
@@ -0,0 +1,12 @@
+package aula4prog3;
+
+public class Main {
+
+	public static void main(String [] args){
+		
+		Empregado e = new Empregado("Joao", "da Silva", 1, 1, 2000, 2, 2, 2021);
+		
+		System.out.println(e);
+		
+	}
+}
