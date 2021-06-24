4d1cf228342e9dc05d5d6e2b94ed0e2c82ea6367
simplecloudservlet <outrosdiasvirao@gmail.com>
2021-06-24 10:37:08 -0300

    aula10prog3

---
+++ aula10prog3/src/aula10prog3/Samambaia.java
@@ -0,0 +1,21 @@
+package aula10prog3;
+
+public final class Samambaia extends Planta {
+
+	public Samambaia() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String local() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean vazo() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
