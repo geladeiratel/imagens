4eba8e6acb3a6bbc0003ecf809737cddc480af5f
simplecloudservlet <outrosdiasvirao@gmail.com>
2021-06-24 10:52:44 -0300

    aula5prog1. Update do .gitignore

---
+++ aula5prog1/src/aula5prog1/Principal.java
@@ -0,0 +1,14 @@
+package aula5prog1;
+
+public class Principal {
+
+	public Principal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
