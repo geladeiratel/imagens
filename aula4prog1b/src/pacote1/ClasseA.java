bc4cffdcd9949de9b55e0bb001d0a3b312695819
simplecloudservlet <outrosdiasvirao@gmail.com>
2021-06-24 08:54:55 -0300

    aula4prog1b

---
+++ aula4prog1b/src/pacote1/ClasseA.java
@@ -0,0 +1,18 @@
+package pacote1;
+
+public class ClasseA {
+
+	//Variaveis de instancia	
+	public String primeiro;
+	protected String segundo;
+	String terceiro;
+	private String quarto;	
+	
+	public ClasseA(){
+		this.primeiro = "1";
+		this.segundo = "2";
+		this.terceiro = "3";
+		this.quarto = "4";
+	}
+
+}
